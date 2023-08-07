54044bfe3f538e0d442cf2329d59c36351ab866e
Anntasena <matlailsyahrin@gmail.com>
2023-08-07 21:19:33 +0700

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,29 @@
 // import Logo from './Utilites/Logo2.png'
 
+import Logo2 from './../Utilities/Logo2.png'
+
 function Navbar() {
   return(
     <>
-      <nav className="flex h-[80px] bg-black justify-between items-center text-white">
+      <nav className="flex h-[80px] bg-black justify-between items-center text-white text-2xl geometr ">
         <div className="flex pl-10">
-          <a href="#" className=""><img src={ require('./src/Utilities/Logo2.png')}></img></a>
+          <a href="#" className="w-[150px]"><img src={Logo2} alt='Logo2'/></a>
         </div>
         <div>
           <ul className="flex">
-            <li className=""><a href="#">EVENT</a></li>
-            <li className="pl-9"><a href="#">MERCH</a></li>
-            <li className="pl-9"><a href="#">ABOUT US</a></li>
+            <li className="hover:text-red-600"><a href="# ">EVENT</a></li>
+            <li className="pl-9 hover:text-red-600"><a href="#">MERCH</a></li>
+            <li className="pl-9 hover:text-red-600"><a href="#">ABOUT US</a></li>
           </ul>
         </div>
         <div>
           <ul className="flex">
-            <li><a href="#">LOGIN</a></li>
-            <li className="pl-3 pr-10"><a href="#">SEARCH</a></li>
+            <li><a href="#">SEARCH</a></li>
+            <li className="pl-3 pr-10">
+              <div className='flex items-center  bg-red-600 rounded-lg w-[80px] h-[32px]'>
+                <a href="#" className='pl-2 text-xl'>LOGIN</a>
+              </div>
+            </li>
           </ul>
         </div>
       </nav>
