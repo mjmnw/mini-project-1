3dd277bb4a8a0144a86f022233ba850b22ceb71a
Martin Joman <martinjoman2907@gmail.com>
2023-08-09 12:42:06 +0700

    Terbaru2

--- src/Pages/MerchandisePage/Index.jsx
+++ src/Pages/MerchandisePage/Index.jsx
@@ -1,32 +1,32 @@
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 // import Card from "../../Components/Card";
 
 export default function Merchandise() {
-  const [details, setDetails] = useState({})
-  const {id} = useParams()
+  // const [details, setDetails] = useState({})
+  // const {id} = useParams()
   
-  const onFetchData = async() => {
-      try {
-          const response = await axios.get(`http://localhost:5000/merchandise/${id}`)
-          setDetails(response.data)
-      } catch (error) {
-          console.log("error")
-      }
-  }
+  // const onFetchData = async() => {
+  //     try {
+  //         const response = await axios.get(`http://localhost:5000/merchandise/${id}`)
+  //         setDetails(response.data)
+  //     } catch (error) {
+  //         console.log("error")
+  //     }
+  // }
 
-  // const obj = {name: 'Botol', images: ' ', price: 1, stocks: 1, typeId: 0, id: 0}
-  // useEffect(() => {
-  //     onFetchData()
-  // }, [])
+  // // const obj = {name: 'Botol', images: ' ', price: 1, stocks: 1, typeId: 0, id: 0}
+  // // useEffect(() => {
+  // //     onFetchData()
+  // // }, [])
 
   return (
     <>
       <Navbar />
-      <h1>
+      {/* <h1>
                 Detail Product
             </h1>
             <h1>
@@ -34,7 +34,7 @@ export default function Merchandise() {
             </h1>
             <span>
                 {details?.price}
-            </span>
+            </span> */}
       <Footer />
     </>
   );
--- src/Pages/TermsAndConditionsPage/Index.jsx
+++ src/Pages/TermsAndConditionsPage/Index.jsx
@@ -1,6 +1,9 @@
 export default function TnC () {
     return(
         <>
+        <div className="bg-black">
+
+        </div>
         </>
     )
 }
