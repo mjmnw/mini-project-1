4ad86d87b40ae6c94e285a019bbff9415fb75211
Martin Joman <martinjoman2907@gmail.com>
2023-08-09 12:48:13 +0700

    Terbaru3

--- src/Pages/MerchandisePage/Index.jsx
+++ src/Pages/MerchandisePage/Index.jsx
@@ -1,40 +1,11 @@
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import Card from "../../Components/Card";
 
 export default function Merchandise() {
-  // const [details, setDetails] = useState({})
-  // const {id} = useParams()
-  
-  // const onFetchData = async() => {
-  //     try {
-  //         const response = await axios.get(`http://localhost:5000/merchandise/${id}`)
-  //         setDetails(response.data)
-  //     } catch (error) {
-  //         console.log("error")
-  //     }
-  // }
-
-  // // const obj = {name: 'Botol', images: ' ', price: 1, stocks: 1, typeId: 0, id: 0}
-  // // useEffect(() => {
-  // //     onFetchData()
-  // // }, [])
 
   return (
     <>
       <Navbar />
-      {/* <h1>
-                Detail Product
-            </h1>
-            <h1>
-                {details?.name}
-            </h1>
-            <span>
-                {details?.price}
-            </span> */}
       <Footer />
     </>
   );
--- src/Pages/TermsAndConditionsPage/Index.jsx
+++ src/Pages/TermsAndConditionsPage/Index.jsx
@@ -1,8 +1,15 @@
+import Navbar from "../../Components/Navbar"
+import Footer from "../../Components/Footer"
+
 export default function TnC () {
     return(
         <>
         <div className="bg-black">
-
+        <Navbar />
+        <div className="justify-center px-[500px]  ">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6RBjSr7g_gk4nmI0ofH8lk2rHxKRAx6zerQ&usqp=CAU"/>
+        </div>
+        <Footer />
         </div>
         </>
     )
