231d8f731bc1d4cae92d8c461c458478e389093f
Anntasena <matlailsyahrin@gmail.com>
2023-08-12 16:35:09 +0700

    test

--- src/App.js
+++ src/App.js
@@ -17,21 +17,6 @@ import Register from "./Components/Register";
 import { store } from "./Redux/Store";
 import Login2 from "./Components/Login2";
 
-<<<<<<< HEAD
-import {Routes, Route} from 'react-router-dom';
-import Home from './Pages/HomePage/Index';
-import LoginPage from './Pages/LoginPage/Index';
-import RegisterPage from './Pages/RegisterPage/Index';
-import Merchandise from './Pages/MerchandisePage/Index'
-import MyAccount from './Pages/MyAccountPage/Index';
-import Transaction from './Pages/TransactionPage/Index';
-import TnC from './Pages/TermsAndConditionsPage/Index';
-import AboutUsPage from './Pages/AboutUsPage/Index';
-import MyTicket from './Pages/MyTicketPage/Index';
-import NotFound from './Pages/PageNotFound/Index';
-import ListEvent  from './Pages/ListEventPage/Index';
-=======
->>>>>>> 33efc187ad12c7dd64759dc558c9b8e1982d3aee
 
 // import axios from 'axios';
 // import toast from 'react-hot-toast';
@@ -41,23 +26,6 @@ import ListEvent  from './Pages/ListEventPage/Index';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/login' element={<LoginPage />} />
-    <Route path='/register' element={<RegisterPage />} />
-    <Route path='/merchandise' element={<Merchandise />} />
-    <Route path='/myaccount' element={<MyAccount />} />
-    <Route path='/transaction' element={<Transaction />} />
-    <Route path='/termsandconditions' element={<TnC />} />
-    <Route path='/aboutus' element={<AboutUsPage />} />
-    <Route path='/myticket' element={<MyTicket />} />
-    <Route path='/*' element={<NotFound />} />
-    <Route path='/listevent' element={<ListEvent/>} />
-    </Routes>
-    </>
-=======
     <Provider store={store}>
       <KeepLogin>
         <div className="App">
@@ -78,7 +46,6 @@ function App() {
         </div>
         </KeepLogin>
     </Provider>
->>>>>>> 33efc187ad12c7dd64759dc558c9b8e1982d3aee
   );
 }
 
