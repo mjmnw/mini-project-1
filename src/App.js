b2f1fcd6ad4fa400609dba03ef650abd562d6735
AryaBintangRamadhan <aryabintangr1@gmail.com>
2023-08-14 11:52:34 +0700

    dbjson

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import KeepLogin from "./AppWrapper/KeepLogin";
 import Register from "./Pages/RegisterPage/Index";
 import { store } from "./Redux/Store";
 import Login from "./Pages/LoginPage/Index";
-import TransactionHistory from "./Pages/TransactionHistory/Index";
+// import TransactionHistory from "./Pages/TransactionHistory/Index";
 
 
 // import axios from 'axios';
@@ -40,7 +40,7 @@ function App() {
             <Route path="/aboutus" element={<AboutUsPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/transactionhistory" element={<TransactionHistory />} />
+            {/* <Route path="/transactionhistory" element={<TransactionHistory />} /> */}
             <Route path="/*" element={<NotFound />} />
             </Routes>
         </div>
